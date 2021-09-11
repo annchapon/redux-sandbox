6ddde7d7da8e1dd233766c345c5dad7857d1da77
annchapon <annchapon@gmail.com>
2021-09-11 12:56:38 +0300

    add UI for redux (counter)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const reducer = (state = 0, action) => {
   switch (action.type) {
     case 'INC': 
       return state + 1;
+
+    case 'DEC':
+      return state - 1;
+
     default:
       return state;
   }
@@ -11,6 +15,27 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
+document
+  .getElementById('inc')
+  .addEventListener('click', () => {
+    store.dispatch({type: 'INC'});
+  });
+
+document
+  .getElementById('dec')
+  .addEventListener('click', () => {
+    store.dispatch({type: 'DEC'});
+  });
+
+const update = () => {
+  document
+    .getElementById('counter')
+    .innerHTML = store.getState();
+};
+
+store.subscribe(update);
+
+/*
 store.subscribe(() => {
   console.log('subscribe: ' + store.getState());
 });
@@ -21,3 +46,4 @@ store.dispatch({type: 'INC'});
 store.dispatch({type: 'INC'});
 
 console.log(store.getState());
+*/
