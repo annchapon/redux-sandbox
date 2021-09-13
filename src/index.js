7c07dac02bebef046b8b4d8ca834776d6d36572f
annchapon <annchapon@gmail.com>
2021-09-13 12:42:43 +0300

    add Counter component (use react and redux)

--- src/index.js
+++ src/index.js
@@ -1,68 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore, bindActionCreators } from 'redux';
 
+import Counter from './counter';
+
 import reducer from './reducer';
-/*import { inc, dec, rnd } from './actions';*/
 import * as actions from './actions';
 
 const store = createStore(reducer);
 const { dispatch } = store;
 
-/*
-const bindActionCreator = (creator, dispatch) => (...args) => {
-  dispatch(creator(...args));
-};
-*/
-
-/*
-const incDispatch = bindActionCreators(inc, dispatch);
-const decDispatch = bindActionCreators(dec, dispatch);
-const rndDispatch = bindActionCreators(rnd, dispatch);
-*/
-
-/*
-const { incDispatch, decDispatch, rndDispatch } = 
-  bindActionCreators({
-  incDispatch: inc,
-  decDispatch: dec,
-  rndDispatch: rnd
-}, dispatch);
-*/
-
 const { inc, dec, rnd } = 
   bindActionCreators(actions, dispatch);
 
-document
-  .getElementById('inc')
-  .addEventListener('click', inc);
-
-document
-  .getElementById('dec')
-  .addEventListener('click', dec);
-
-document
-  .getElementById('rnd')
-  .addEventListener('click', () => {
-    const payload = Math.floor(Math.random()*10);
-    rnd(payload);
-  });
-
 const update = () => {
-  document
-    .getElementById('counter')
-    .innerHTML = store.getState();
+  ReactDOM.render(
+    <Counter 
+      counter={store.getState()} 
+      inc={inc} 
+      dec={dec} 
+      rnd={() => {
+        const value = Math.floor(Math.random()*10);
+        rnd(value);
+      }} />, 
+    document.getElementById('root'));
 };
 
+update();
 store.subscribe(update);
-
-/*
-store.subscribe(() => {
-  console.log('subscribe: ' + store.getState());
-});
-
-console.log(store.getState());
-
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-
-console.log(store.getState());
-*/
